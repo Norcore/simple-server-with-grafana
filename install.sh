cce86d33aacab71a7ee1800ba0377b85a32a7e80
Norcore <norbertrichter94@gmail.com>
2024-03-15 11:41:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+
 # Add Helm repositories
 helm repo add prometheus-community https://prometheus-community.github.io/helm-chartshelm 
 repo add stable https://charts.helm.sh/stable 
@@ -9,9 +12,11 @@ helm upgrade --install prometheus-operator prometheus-community/kube-prometheus-
 # Wait for Prometheus and Grafana to be ready
 kubectl rollout status deployment/simple-server-with-grafana-deployment --namespace default
 kubectl rollout status deployment/prometheus-operator-grafana --namespace default
+kubectl rollout status pod/prometheus-prometheus-operator-kube-p-prometheus-0 --namespace default
 
 # Set up port forwarding
 kubectl port-forward deployment/simple-server-with-grafana-deployment 5000:5000 --namespace default &
 kubectl port-forward deployment/prometheus-operator-grafana 3000:3000 --namespace default &
+kubectl port-forward svc/prometheus-operator-kube-p-prometheus 9090:9090 -n default &
 
-echo "Prometheus and Grafana are now accessible. Prometheus: http://localhost:5000, Grafana: http://localhost:3000"
+echo "Prometheus and Grafana are now accessible. Web app: http://localhost:5000, Grafana: http://localhost:3000", Prometheus: http://localhost:9090
